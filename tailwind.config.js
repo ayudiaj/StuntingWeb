94cd61b8b64fe118d2bd33d853cb86294c241c9c
AyudiaJayanti <ayudiajayanti05@gmail.com>
2023-12-18 17:38:57 +0700

    add new section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: [
+    'index.html',
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     colors: {
       'primary100' : '#FFF9D9',
